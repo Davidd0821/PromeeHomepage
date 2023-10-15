c29cd0b9069ee422fcef4da190ad76e0f900ae06
Davidd0821 <126388661+Davidd0821@users.noreply.github.com>
2023-10-15 16:16:32 +0700

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿
+using Promee_Homepage.Model;
+
+namespace Promee_Homepage;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+		
+		MainPage = new AppShell();  
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Promee_Homepage.Model;
+
+namespace Promee_Homepage;
+
+public partial class MainPage : ContentPage
+{
+
+
+	public MainPage()
+	{
+		InitializeComponent();
+
+        var items = new List<CollectionItem>
+        {
+            new CollectionItem {Title = "Welcome to Backlog", Description = "as"},
+            new CollectionItem {Title = "Welcome to Doing", Description = "as"},
+            new CollectionItem {Title = "Welcome to Done", Description = "as"}
+        };
+        CarView.ItemsSource = items;
+
+        
+
+    }
+
+	
+}
+
